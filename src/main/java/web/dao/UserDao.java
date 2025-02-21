72c0b638058d8d875c248e75fdf813c362b7d1e5
Born24 <106234496+Born24@users.noreply.github.com>
2025-02-21 22:14:26 +0300

    first commit

---
+++ src/main/java/web/dao/UserDao.java
@@ -0,0 +1,20 @@
+package web.dao;
+
+import web.model.User;
+
+import java.util.List;
+
+
+public interface UserDao {
+
+    List<User> findAll();
+
+    User findById(Long id);
+
+    void add(User user);
+
+    void update(User user);
+
+    void delete(Long id);
+
+}
